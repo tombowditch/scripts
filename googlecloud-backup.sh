95910f2bf09fab7949d807240e7558094e53487b
madyoda <tom@yoda.pw>
2017-02-07 17:44:37 +0000

    Google Cloud Storage backup script

---
+++ googlecloud-backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Backup script to Google Cloud Storage
+# Requires gsutil (https://cloud.google.com/storage/docs/gsutil) to be initialized 
+
+
+# CONFIG #
+
+# Set your backup locations here (folders)
+
+backup_locations=('/var/www' '/etc')
+google_bucket="backups"
+
+# END CONFIG #
+
+function run_backup {
+  gsutil -m rsync -e -r $1 gs://$google_bucket$1
+}
+
+function backup {
+  echo "Running backup: $1 | $(date)"
+  run_backup $1
+  echo "Backup ran: $1"
+}
+
+for loc in "${backup_locations[@]}"
+do
+	backup $loc
+done
+
+echo "Backup completed successfully at $(date)"
