92e4031543b905b65f1a81651df21ea4e8df3ead
madyoda <tom@yoda.pw>
2017-03-12 17:57:14 +0000

    Massive update to acd script. Works flawlessly now and is much faster. Also switched to rclone.

--- backup-scripts/amazon-cloud-drive/README.md
+++ backup-scripts/amazon-cloud-drive/README.md
@@ -1,3 +1,8 @@
 # Amazon Cloud Drive Backup  
 
-![acd image](http://i.imgur.com/j35DjDJ.png)
+![acd image](http://i.imgur.com/j35DjDJ.png)  
+
+# Changelog  
+
+* 12/03/2017: Change acd-cli to rclone  
+* 12/03/2017: Change zip to tar  
--- backup-scripts/amazon-cloud-drive/acd-backup.sh
+++ backup-scripts/amazon-cloud-drive/acd-backup.sh
@@ -1,25 +1,30 @@
 # /bin/bash
-# Backup script to Amazon Cloud Drive
-# Requires acd_cli to be initialized 
+# Backup script to Amazon Cloud Drive using rclone
+# Requires rclone to be configured and installed (checks are in place) 
 
 set -u
-VERSION="1.0"
+VERSION="2.0"
 
 # CONFIG #
+
+# Name of server (leave it how it is to use the hostname of the server)
+name=$(hostname)
 
 # Set your backup locations here (folders)
 backup_locations=('/var/www' '/etc')
 
-# UNTESTED SO FAR
 exclude=n # set to 'y' to enable exclusions
 exclusions=('/var/www/somebigfolder' '/another/folder')
-
-password_protect=n # set to 'y' to enable password protection on the zip
-password="" # password to lock zip with
 
 telegram=n # set to 'y' to enable telegram messages
 telegram_chat_id=""
 telegram_bot_token=""
+
+pushover=n # set to 'y' to enable pushover messages
+pushover_app_token=""
+pushover_user_key=""
+
+rclone_destination_name="acd" # the name of your rclone destination (viewable in .rclone.conf as [name] )
 
 # END CONFIG #
 
@@ -27,11 +32,15 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-function send_tg {
+function send_message {
 	echo "$1"
 	if [ $telegram = "y" ]; then
 		curl --header 'Content-Type: application/json' --data-binary '{"chat_id":"'"$telegram_chat_id"'","text":"<code>'"$1"'</code>","parse_mode":"html"}' --request POST https://api.telegram.org/bot${telegram_bot_token}/sendMessage >> /dev/null
 	fi
+
+  if [ $pushover = "y" ]; then
+    curl -s --form-string "token=$pushover_app_token" --form-string "user=$pushover_user_key" --form-string "message=$1" https://api.pushover.net/1/messages.json >> /dev/null
+  fi
 }
 
 function msg {
@@ -57,85 +66,123 @@ function greentext {
   echo -e "${GREEN}$1${NC}"
 }
 
+function install_rclone {
+  msg "RCLONE INSTALL" "Installing rclone..."
+  curl -O http://downloads.rclone.org/rclone-current-linux-amd64.zip
+  unzip rclone-current-linux-amd64.zip
+  cd rclone-*-linux-amd64
+  cp rclone /usr/sbin/
+  chown root:root /usr/sbin/rclone
+  chmod 755 /usr/sbin/rclone
+  mkdir -p /usr/local/share/man/man1
+  cp rclone.1 /usr/local/share/man/man1/
+  mandb 
+  msg "RCLONE INSTALL" "Installed rclone."
+  msg "RCLONE INSTALL" "If you don't already have a preexisting .rclone.conf file, run 'rclone config'"
+}
+
 #Start script
 if [[ $EUID -ne 0 ]]; then
-   msgred "WARN" "You are not running this script as root; make sure you have access to all directories"
+   msgred "INFO" "You are not running this script as root; make sure you have access to all directories"
 fi
 
-if [ -f ~/.cache/acd_cli/oauth_data ]
-then
-	msg "INFO" "Valid acd_cli install found"
+rcloneexec=$(which rclone)
+
+function check_rclone {
+  if [ -z "$rcloneexec" ]; then
+    msgred "FATAL" "rclone is not installed or not found"
+    if [[ $EUID -ne 0 ]]; then
+      msgred "FATAL" "Please install it and run again"
+      msgred "FATAL" "Install here: http://rclone.org/install/"
+      exit 1
+    else
+      msgred "FATAL" "Would you like to install it? (y/n)"
+      read shouldinstall
+      if [ "$shouldinstall" == "y" ]; then
+        install_rclone
+
+        rcloneexec=$(which rclone)
+        check_rclone
+      else
+        msgred "FATAL" "Exiting"
+        msgred "FATAL" "Install here: http://rclone.org/install/"
+        exit 1
+      fi
+    fi
+  else
+    msg "INFO" "rclone executable found: $rcloneexec"
+  fi
+}
+
+check_rclone
+
+if [ -f ~/.rclone.conf ]; then
+	msg "INFO" "rclone config found"
 else
 	echo ""
-	msgred "FATAL" "No oauth_data found - are you sure acd_cli is installed and initialized?"
+	msgred "FATAL" "No rclong config found - are you sure rclone is installed and initialized?"
 	msgred "FATAL" "Sleeping for 6 seconds..."
 	sleep 6
 fi
 
 msg "INFO" "Checking we have needed packages installed"
-pkgcheck zip
 pkgcheck tar
-pkgcheck ncurses-term
 echo ""
 echo -e "${RED}YodaBackup $VERSION${NC} - $(date)"
 echo ""
 msg "INFO" "Script starting"
 echo ""
 
-HOST=$(hostname)
+HOST=$name
 TMPDIR="/tmp/acdbackup"
 rm -r $TMPDIR
 mkdir -p $TMPDIR
 DATE=$(date +"%Y.%m.%d-%H:%M:%S")
 
-send_tg "[$(hostname)] [$DATE] [BACKUP] [INFO] Starting backup"
-msg "ACD" "Making needed ACD files and folders..."
-/usr/local/bin/acdcli create backup
-/usr/local/bin/acdcli create backup/$HOST
-/usr/local/bin/acdcli create backup/$HOST/$DATE
+send_message "[${name}] [$DATE] [BACKUP] [INFO] Starting backup"
+msg "ACD" "Making needed folders..."
+$rcloneexec mkdir ${rclone_destination_name}:/backup/$HOST/$DATE
 msg "BACKUP" "Starting backups..."
 msgred "WARN" "Please do not interrupt"
 echo ""
 
 excl_list=""
-if [ $exclude = "y" ]; then
-	excl_list="-x "
+if [ $exclude == "y" ]; then
 	msg "INFO" "Calculating exclusions..."
 	for excl in "${exclusions[@]}"
 	do
-		excl_list="$excl_list \"$excl\" "
+		excl_list="$excl_list --exclude=${excl} "
 	done
 fi
-
-pass_arg=""
-if [ $password_protect = "y" ]; then
-	pass_arg="-P $password"
-fi
-
-echo ""
 
 for loc in "${backup_locations[@]}"
 do
   msg "BACKUP" "Starting backup $loc"
+
+  # Generating name for the file
   echo "$loc" > .yodabackup_tmp
   sed -i 's/\//./g' .yodabackup_tmp
   FILENAME=$(cat .yodabackup_tmp)
-
   rm .yodabackup_tmp
-  ZIPNAME="$HOST-$DATE-$FILENAME.zip"
-  ZIPLOC="$TMPDIR/$ZIPNAME"
-  zip $pass_arg -r "$ZIPLOC" "$loc" $excl_list
-  msg "BACKUP" "Zipped $loc"
-  msg "BACKUP" "Uploading $ZIPNAME"
-  /usr/local/bin/acdcli upload -x 50 $ZIPLOC backup/$HOST/$DATE
-  msg "BACKUP" "Uploaded $ZIPNAME to ACD: backup/$HOST/$DATE/$ZIPNAME"
+  TARNAME="$HOST-$DATE-$FILENAME.tar.gz"
+  TARLOC="$TMPDIR/$TARNAME"
+
+  echo "tar $excl_list -zcvf \"$TARLOC\" \"$loc\""
+  tar $excl_list -zcvf "$TARLOC" "$loc"
+  msg "BACKUP" "Tar'd $loc"
+  msg "BACKUP" "Uploading $TARNAME"
+
+  $rcloneexec --stats=10s --transfers=2 --checkers=15 move $TARLOC ${rclone_destination_name}:/backup/$HOST/$DATE
+  msg "BACKUP" "Uploaded $TARNAME to ACD: backup/$HOST/$DATE/$TARNAME"
 done
 echo ""
-msg "INFO" "Script finished, enjoy"
-/usr/local/bin/acdcli ls backup/$HOST/$DATE
+msg "INFO" "Script finished"
 FINISH_DATE=$(date +"%Y.%m.%d-%H:%M:%S")
 
-send_tg "[$(hostname)] [$FINISH_DATE] [BACKUP] [INFO] Finished backup\n $(/usr/local/bin/acdcli ls backup/$HOST/$DATE)"
+# Nicely formatting output list
+OUT=$($rcloneexec ls ${rclone_destination_name}:/backup/$HOST/$DATE | awk '{print $2}' | awk '$1=$1' ORS=' ')
+
+send_message "[${name}] [$FINISH_DATE] [BACKUP] [INFO] Finished backup: $OUT"
 
 msg "CLEANUP" "Cleaning up temporary directories"
 rm -r $TMPDIR
